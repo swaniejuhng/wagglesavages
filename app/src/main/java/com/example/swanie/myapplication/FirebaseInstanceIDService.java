c66e465a799fa5f4d4759f1c323741ffe5d1c871
SWANIEPC\Swanie <swaniejuhng@gmail.com>
2018-08-04 04:41:16 +0900

    Generate new Android package

---
+++ app/src/main/java/com/example/swanie/myapplication/FirebaseInstanceIDService.java
@@ -0,0 +1,39 @@
+package com.example.swanie.myapplication;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import java.io.IOException;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        registerToken(token);
+    }
+    private void registerToken(String token) {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Token", token)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://192.168.1.71/fcm/register.php")
+                .build();
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/swanie/myapplication/Notifi.java
@@ -0,0 +1,4 @@
+package com.example.swanie.myapplication;
+
+public class Notifi {
+}
